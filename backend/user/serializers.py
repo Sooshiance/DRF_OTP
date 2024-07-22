18cd76f85a9c4abdee132fd522aedf56c1e1eb4e
sooshiance <s.katafract@gmail.com>
2024-07-22 15:47:46 +0330

    build: Create all serializers that we need

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,3 +1,26 @@
 from rest_framework import serializers
 
+from .models import User, Profile
 # Create your serializers here.
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['phone', 'email', 'first_name', 'last_name']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+
+class TokenSerializer(serializers.Serializer):
+    phone    = serializers.CharField()
+    password = serializers.CharField(required=False)
+
+
+class OTPSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    phone = serializers.CharField()
