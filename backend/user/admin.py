f9275729f0237c99b92977abd0530d36ad9e59bf
sooshiance <s.katafract@gmail.com>
2024-07-22 15:37:47 +0330

    feat: Register User model into admin panel

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Profile, OTP
+
+
+class Admin(UserAdmin):
+    list_display = ('pk', 'phone', 'superuser')
+    filter_horizontal = ()
+    list_filter = ('superuser', )
+    fieldsets = ()
+    ordering = ('pk', )
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone')
+
+
+admin.site.register(User, Admin)
+
+admin.site.register(Profile, ProfileAdmin)
+
+admin.site.register(OTP)
