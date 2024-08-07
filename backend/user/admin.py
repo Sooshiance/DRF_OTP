5e6d1671804480962be23e215b993a0b5df6b99c
sooshiance <s.katafract@gmail.com>
2024-08-07 19:04:30 +0330

    fix: Add UserCreationForm to Admin class

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -2,14 +2,31 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Profile, OTP
+from .forms import UserCreationForm
 
 
 class Admin(UserAdmin):
-    list_display = ('pk', 'phone', 'is_superuser')
+    add_form = UserCreationForm
+    list_display = ('id', 'phone', 'last_name')
+    list_display_links = ['phone', 'id']
     filter_horizontal = ()
-    list_filter = ('is_superuser', )
-    fieldsets = ()
-    ordering = ('pk', )
+    list_filter = []
+    search_fields = ['phone', 'fullName']
+    ordering = ['id', 'last_name']
+
+    fieldsets = (
+    (None, {'fields': ('phone',)}),
+    ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+    (None, {
+    'classes': ('wide',),
+    'fields': ('phone', 'first_name', 'last_name', 'email'),
+    }),
+    )
 
 
 class ProfileAdmin(admin.ModelAdmin):
